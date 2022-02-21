1d2d91e10b814867754a54d90729c2bd28651842
comradeskolopendra <91865240+comradeskolopendra@users.noreply.github.com>
2022-02-21 10:39:37 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,8 @@ let arrow_down = document.querySelector('#arrow_down')
 
 drop_down.addEventListener('click', (event) => {
     event.preventDefault();
-    if (social.classList.contains('display__none')) {
-        arrow_down.classList.toggle('arrow__animation')
-        social.classList.add('animation__down')
-        social.classList.remove('animation__up')
-    }
-    else if (!social.classList.contains('display__none')) {
-        arrow_down.classList.toggle('arrow__animation')
-    }
-    social.classList.toggle('display__none')
+    social.classList.toggle('animate')
+    arrow_down.classList.toggle('arrow__animate')
 })
 
 button_back.addEventListener('click', () => {
